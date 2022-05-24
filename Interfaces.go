f1bc40fefab3c2fa38b0467b54c5cb560e23c636
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-25 03:34:13 +0430

    comments

--- Interfaces.go
+++ Interfaces.go
@@ -1,14 +1,20 @@
 package main
 
+//Interfaces are named collections of method signatures.
+
 import (
 	"fmt"
 	"math"
 )
 
+//Here’s a basic interface for geometric shapes.
+
 type geometry interface {
 	area() float64
 	perim() float64
 }
+
+//For our example we’ll implement this interface on rect and circle types
 
 type rect struct {
 	width, height float64
@@ -23,6 +29,10 @@ func (r rect) area() float64 {
 func (r rect) perim() float64 {
 	return 2*r.width + 2*r.height
 }
+
+//To implement an interface in Go, we just need to implement all the methods in the interface.
+//Here we implement geometry on rects.
+//The implementation for circles.
 
 func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
@@ -44,3 +54,19 @@ func main() {
 	measure(r)
 	measure(c)
 }
+
+//If a variable has an interface type, then we can call methods that
+//are in the named interface. Here’s a generic measure
+// function taking advantage of this to work on any geometry.
+
+//The circle and rect struct types both implement the geometry
+// interface so we can use instances of these structs as arguments to measure.
+
+//output :
+
+//{3 4}
+//12
+//14
+//{5}
+//78.53981633974483
+//31.41592653589793
